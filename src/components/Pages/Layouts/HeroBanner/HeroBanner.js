f9a8fc23ef6eb6c22694a074a9ca62d92d555443
imhasibulhasan <hashibul218@gmail.com>
2021-12-19 00:26:27 +0600

    add hero section

--- src/components/Pages/Layouts/HeroBanner/HeroBanner.js
+++ src/components/Pages/Layouts/HeroBanner/HeroBanner.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Button, FormControl, InputGroup } from 'react-bootstrap';
 import './HeroBanner.css';
 
 const HeroBanner = () => {
     return (
-        <div>
-            <h3>This hero HeroBanner</h3>
+        <div className="hero mb-3">
+            <img src="https://i.ibb.co/LpGKhJx/bannerbackground.png" alt="" />
+            <div className="search">
+                <h2 className="hero-title">Best Food waiting for  your belly.</h2><br />
+                <div className="position-relative">
+                    <input className="form-control rounded-pill border-0" type="text" placeholder='Search your food...' />
+                    <Button className="rounded-pill search-btn">Search</Button>
+                </div>
+            </div>
+
         </div>
     );
 };
